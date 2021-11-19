db90011685303a09c2aa525998e595cac827a91b
Gurgen-Arm <gugo151001@gmail.com>
2021-11-19 21:47:13 +0300

    1

---
+++ modules/task_1/harutyunyan_g_rectangle_method_integration/main.cpp
@@ -0,0 +1,90 @@
+// Copyright 2018 Nesterov Alexander
+#include <gtest/gtest.h>
+#include <cmath>
+#include "./rectangle_method_integration.h"
+#include <gtest-mpi-listener.hpp>
+
+TEST(Paralel_Operations_MPI, x_squad)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int a = 1,  b = 10, n = 100;
+    const std::function<double(double)>f = [](double x) {return x*x; };
+    double sum = getParallelOperations(a, b, n, f);
+    
+    if (rank == 0)
+    {
+        double seq_sum = getSequentialOperations(a, b, n, f);
+        ASSERT_DOUBLE_EQ(seq_sum, sum);
+    }
+}
+TEST(Paralel_Operations_MPI, x_lnx)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int a = 2, b = 5, n = 100;
+    const std::function<double(double)>f = [](double x) {return 1/log(x); };
+    double sum = getParallelOperations(a, b, n, f);
+    
+    if (rank == 0)
+    {
+        double seq_sum = getSequentialOperations(a, b, n, f);
+        ASSERT_DOUBLE_EQ(seq_sum, sum);
+    }
+}
+TEST(Paralel_Operations_MPI, x_sin)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int a = 0, b = 1, n = 100;
+    const std::function<double(double)>f = [](double x) {return (1-sin(x)); };
+    double sum = getParallelOperations(a, b, n, f);
+    
+    if (rank == 0)
+    {
+        double seq_sum = getSequentialOperations(a, b, n, f);
+        ASSERT_DOUBLE_EQ(seq_sum, sum);
+    }
+}
+TEST(Paralel_Operations_MPI, x_cos)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int a = 0, b = 1, n = 100;
+    const std::function<double(double)>f = [](double x) {return (cos(x)-1); };
+    double sum = getParallelOperations(a, b, n, f);
+    
+    if (rank == 0)
+    {
+        double seq_sum = getSequentialOperations(a, b, n, f);
+        ASSERT_DOUBLE_EQ(seq_sum, sum);
+    }
+}
+TEST(Paralel_Operations_MPI, x_cube)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int a = -3, b = 9, n = 100;
+    const std::function<double(double)>f = [](double x) {return x * x*x; };
+    double sum = getParallelOperations(a, b, n, f);
+   
+    if (rank == 0)
+    {
+        double seq_sum = getSequentialOperations(a, b, n, f);
+        ASSERT_DOUBLE_EQ(seq_sum, sum);
+    }
+}
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+ 
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/harutyunyan_g_rectangle_method_integration/rectangle_method_integration.cpp
@@ -0,0 +1,39 @@
+// Copyright 2018 Nesterov Alexander
+#include <mpi.h>
+#include <iostream>
+#include <functional>
+#include "../../../modules/task_1/harutyunyan_g_rectangle_method_integration/rectangle_method_integration.h"
+
+double getSequentialOperations(double a, double b, int n, const std::function<double(double)>& f)// b-верхний предел интегрирования,а- нижний предел
+{
+	const double step = (b - a) / n;
+	double rectangle_integration = 0;
+	for (int i= 0; i < n; i++)
+	{
+		double x_1 = a + i * step;
+		double x_2 = x_1 + step / 2;
+		rectangle_integration += step * f(x_2);
+
+	}
+	
+	return rectangle_integration;
+
+}
+
+double getParallelOperations(double a, double b, int n, const std::function<double(double)>& f)
+{
+	int size, rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	double res_sum = 0;
+	double sum ;
+	const double step = (b - a) / n;
+	for (int i = 0; i < n; i++)
+	{
+		double x_1 = a + i * step;
+		double x_2 = x_1 + step / 2;
+		res_sum += step * f(x_2);
+	}
+	MPI_Reduce(&res_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	return sum;
+}
