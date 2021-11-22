34ad326a53675d6884f0031ffb9406f7c6243cd1
Gurgen-Arm <gugo151001@gmail.com>
2021-11-22 19:19:35 +0300

    Fix

--- modules/task_1/harutyunyan_g_rectangle_method_integration/rectangle_method_integration.h
+++ modules/task_1/harutyunyan_g_rectangle_method_integration/rectangle_method_integration.h
@@ -3,8 +3,8 @@
 #define MODULES_TASK_1_HARUTYUNYAN_G_RECTANGLE_METHOD_INTEGRATION_RECTANGLE_METHOD_INTEGRATION_H_
 
 double getSequentialOperations(double a, double b, int n,
-    const std::function<double(double)>& f);
+                               const std::function<double(double)>& f);
 double getParallelOperations(double a, double b, int n,
-    const std::function<double(double)>& f);
+                             const std::function<double(double)>& f);
 
 #endif  // MODULES_TASK_1_HARUTYUNYAN_G_RECTANGLE_METHOD_INTEGRATION_RECTANGLE_METHOD_INTEGRATION_H_
